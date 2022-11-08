63bba4351fe959bcd7070c96377a4525569d81ec
ishmyles <33308871+ishmyles@users.noreply.github.com>
2022-11-08 23:46:00 +0000

    User can upload files & return file metadata

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 let express = require('express');
+const multer  = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 let cors = require('cors');
 require('dotenv').config()
 
@@ -11,8 +14,11 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) return res.json({Error: "Please select a file to upload."});
+  const { originalname, mimetype, size } = req.file;
+  res.json({name: originalname, type: mimetype, size: size});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
